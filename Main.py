430a91d2f57760799dc45fb5f260bde94370059d
Dharshan-Delwin <110010704+Dharshan-Delwin@users.noreply.github.com>
2022-09-06 23:45:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,86 +1,158 @@
+
 from typing import Optional
 
+ 
+
 class Node:
-  def __init__(self,data):
-    self.data = data
-    self.next = None
 
-class Link:
-  def __init__(self):
-    self.head = None
-    self.count = 0
-  
-  def push(self,data):
-    new = Node(data)
-    new.next = self.head
-    self.head = new
-    self.count += 1
-  
-  def search(self,target):
-    n = self.head
-    result = 0
-    node_number = 0
-    new = {}
-    for i in range(self.count):
-      if(n.data == target):
-        new = {
-            "postion:":i,
-            "node value:":target
-        }
-    print()
-    return new
+ 
 
-        
+    def __init__(self, data=None, next=None):
 
-  def pair(self):
-    n = self.head
-    while(n):
-      print(n.data,end = "->")
-      n = n.next
-    print("\n")
-    
-  def delete_beg(self):
-    temp = self.head
-    self.head = self.head.next
-    temp = None
-  
-  def delete_end(self):
-    for i in range(self.count):
-      if(i == self.count - 1):
-        temp = self.head
-        self.head.next = None
-        temp = None
+        """
 
+        Initialises the Node with given attributes
 
-  def reverse(self):
-    curr , prev , next = self.head , None,None
-    while(curr != None):
-      next = curr.next
-      curr.next = prev
-      prev = curr
-      curr = next
-    self.head = prev
+        """
 
-l1 = Link()
-# while(1):
-#   x = int(input())
-#   if(x == 1):
-#     val = int(input())
-#     l1.push(val)
-#   elif(x == 2):
-#     l1.reverse()
-#   elif(x == 3):
-#     l1.pair()
-#   elif(x == 4):
-#     search = int(input())
-#     print(l1.search(search))
+        self.data = data
 
-for i in range(3):
-  val = int(input())
-  l1.push(val)
+        self.next = next
 
-l1.pair()
-l1.delete_beg()
-l1.pair()
-l1.delete_end()
-l1.pair()
+ 
+
+ 
+
+class LinkedList:
+
+ 
+
+    def __init__(self):
+
+        """
+
+        Initialize the head
+
+        """
+
+        self.head = None
+
+ 
+
+    def insert_at_end(self, data):
+
+        new = Node(data, None)
+
+        current = self.head
+
+        if current is None:
+
+            self.head = new
+
+        else:
+
+            while current.next is not None:
+
+                current = current.next
+
+            current.next = new
+
+ 
+
+    def status(self):
+
+        elements = []
+
+        current = self.head
+
+        while current:
+
+            elements.append(current.data)
+
+            current = current.next
+
+        print(elements)
+
+ 
+
+ 
+
+class Solution:
+
+ 
+
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
+
+        result = self.get_num(first_list) + self.get_num(second_list)
+
+        sum_list = LinkedList()
+
+        for digit in list(map(int, str(result)[::-1])):
+
+            sum_list.insert_at_end(digit)
+
+        return sum_list
+
+ 
+
+    def get_num(self, l: Optional[LinkedList]) -> int:
+
+        curr = l.head
+
+        if curr is None:
+
+            return 0
+
+        num = ""
+
+        while curr is not None:
+
+            num = str(curr.data) + num
+
+            curr = curr.next
+
+        return int(num)
+
+ 
+
+# Do not edit the following code     
+
+# Create an instance for LinkedList
+
+first_list = LinkedList()
+
+# Create an another instance for LinkedList
+
+second_list = LinkedList()
+
+# Read data for first list
+
+data_for_first_list = list(map(int, input().strip().split(" ")))
+
+# Add data at the end of first_list
+
+for data in data_for_first_list:
+
+    first_list.insert_at_end(data)
+
+# Read data for second list
+
+data_for_second_list = list(map(int, input().strip().split(" ")))
+
+# Add data at the end of second_list
+
+for data in data_for_second_list:
+
+    second_list.insert_at_end(data)
+
+# Create an instance for Solution
+
+solution = Solution()
+
+# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
+
+new_list = solution.addTwoNumbers(first_list, second_list)
+
+# Display the status of new_list
+
+new_list.status()
